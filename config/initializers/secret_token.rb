84272bfd4653026711a33866ecb0130a6d49bc9d
Trey Dockendorf <treydock@gmail.com>
2012-07-16 14:05:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClusterMgmt::Application.config.secret_token = '33b709ee391d29add2ae7f4c2a74d38dff639eaca8b185173641b352fdc9d1c3c4822c4adb7e213d8533c190a6df0af86317ab67e8e44784be10d7a0ee325b0a'
