f2b8338ff2cbc74f53904383d8333381495cfdf3
Trey Dockendorf <treydock@gmail.com>
2012-08-23 11:44:18 -0500

    Revert "- Reload rails_admin while in development (https://github.com/sferik/rails_admin/wiki/How-to%3A-Reloading-RailsAdmin-Config-Automatically)"
    This reverts commit 0394c4f8291c60c47ebfd78ea8c295b06f9b7769.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
 #  before_filter :authenticate_user!
-
-  before_filter :reload_rails_admin, if: :rails_admin_path?
-
+  
   private
-
-    def reload_rails_admin
-      models = ActiveRecord::Base.models
-      models.each do |m|
-        RailsAdmin::Config.reset_model(m)
-      end
-      RailsAdmin::Config::Actions.reset
-
-      load("#{Rails.root}/config/initializers/rails_admin.rb")
-    end
-
-    def rails_admin_path?
-      controller_path =~ /rails_admin/ && Rails.env == "development"
-    end
+  
 end
